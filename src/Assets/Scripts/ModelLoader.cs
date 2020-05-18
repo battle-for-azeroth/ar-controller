224cf933f07202a5dff352edf2ef7016449d18ee
Matias Jensen <sejemus@yahoo.dk>
2020-05-18 20:48:04 +0200

    Added dynamic models

---
+++ src/Assets/Scripts/ModelLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelLoader : MonoBehaviour
+{
+    public GameObject[] models;
+    public int loadModelIndex = 0;
+
+    void Start()
+    {
+        Instantiate(models[loadModelIndex], transform);
+    }
+}
---
+++ src/Assets/Scripts/SpawnPathParts.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SpawnPathParts : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform anchor;
+    public int amountToSpawn = 20;
+
+    void Start()
+    {
+        for (int i = 0; i < amountToSpawn; i++)
+        {
+            Instantiate(prefab, anchor);
+        }
+    }
+}
